42f77869a15f1a623d23d0090697cacc4728731c
yigithanion <55713249+hanion@users.noreply.github.com>
2023-07-11 15:37:08 +0300

    formatting

--- engine/include/log.h
+++ engine/include/log.h
@@ -1,27 +1,23 @@
 #pragma once
 
-#include <memory>
-#include <spdlog/spdlog.h>
 #include <spdlog/sinks/stdout_color_sinks.h>
+#include <spdlog/spdlog.h>
+#include <spdlog/fmt/ostr.h>
 
 namespace Enik {
 
-	class Log {
+class Log {
 
-	public:
-		static void Init();
-		static void hello();
+public:
+	static void Init();
 
-		inline static std::shared_ptr<spdlog::logger>& GetCoreLogger() { return s_CoreLogger; }
-		inline static std::shared_ptr<spdlog::logger>& GetClientLogger() { return s_ClientLogger; }
+	inline static std::shared_ptr<spdlog::logger>& GetCoreLogger() { return s_CoreLogger; }
+	inline static std::shared_ptr<spdlog::logger>& GetClientLogger() { return s_ClientLogger; }
 
-
-	private:
-		static std::shared_ptr<spdlog::logger> s_CoreLogger;
-		static std::shared_ptr<spdlog::logger> s_ClientLogger;
-
-
-	};
+private:
+	static std::shared_ptr<spdlog::logger> s_CoreLogger;
+	static std::shared_ptr<spdlog::logger> s_ClientLogger;
+};
 }
 
 // Core log macros 
--- engine/src/log.cpp
+++ engine/src/log.cpp
@@ -1,16 +1,15 @@
 #include <log.h>
 
-
 namespace Enik {
-	std::shared_ptr<spdlog::logger> Log::s_CoreLogger;
-	std::shared_ptr<spdlog::logger> Log::s_ClientLogger;
+std::shared_ptr<spdlog::logger> Log::s_CoreLogger;
+std::shared_ptr<spdlog::logger> Log::s_ClientLogger;
 
-	void Log::Init(){
-		spdlog::set_pattern("%^[%T] %n: %v%$");
-		s_CoreLogger = spdlog::stdout_color_mt("ENIK");
-		s_CoreLogger->set_level(spdlog::level::trace);
+void Log::Init() {
+	spdlog::set_pattern("%^[%T] %n: %v%$");
+	s_CoreLogger = spdlog::stdout_color_mt("ENIK");
+	s_CoreLogger->set_level(spdlog::level::trace);
 
-		s_ClientLogger = spdlog::stdout_color_mt("APP");
-		s_ClientLogger->set_level(spdlog::level::trace);
-	}
+	s_ClientLogger = spdlog::stdout_color_mt("APP");
+	s_ClientLogger->set_level(spdlog::level::trace);
+}
 }
