6b6291351da11a8e5e926c78a2dac1fc9f484776
yigithanion <55713249+hanion@users.noreply.github.com>
2023-07-14 15:35:46 +0300

    created an example layer

--- sandbox/src/sandbox_app.cpp
+++ sandbox/src/sandbox_app.cpp
@@ -1,19 +1,44 @@
 #include <Enik.h>
 
+class ExampleLayer : public Enik::Layer {
+private:
+	
+public:
+	ExampleLayer()
+		: Layer("Example") {
+		
+	}
+	
+
+	void OnUpdate() override {
+		//EN_INFO("ExampleLayer::OnUpdate");
+	}
+
+
+	void OnEvent(Enik::Event& event) override {
+		EN_TRACE("ExampleLayer::OnEvent {0}", event);
+	}
+
+
+};
+
+
+
 
 class Sandbox : public Enik::Application {
 public:
 	Sandbox(){
 		EN_TRACE("Sandbox Created");
+		PushLayer(new ExampleLayer());
 	}
 
 	~Sandbox(){
 		EN_TRACE("Sandbox Deleted");
 	}
 
-	void Run() override {
-		EN_TRACE("Sandbox Run");
-	}
+	// void Run() override {
+	// 	EN_TRACE("Sandbox Run");
+	// }
 };
 
 
