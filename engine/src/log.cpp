7047325512b7a847f1abc6c1aaae26ceb5ad8433
yigithanion <55713249+hanion@users.noreply.github.com>
2023-07-14 15:36:40 +0300

    Update log.cpp
    much cleaner

--- engine/src/log.cpp
+++ engine/src/log.cpp
@@ -6,7 +6,7 @@ std::shared_ptr<spdlog::logger> Log::s_ClientLogger;
 
 void Log::Init() {
 	spdlog::set_pattern("%^[%T] %n: %v%$");
-	s_CoreLogger = spdlog::stdout_color_mt("ENIK");
+	s_CoreLogger = spdlog::stdout_color_mt("EE");
 	s_CoreLogger->set_level(spdlog::level::trace);
 
 	s_ClientLogger = spdlog::stdout_color_mt("APP");
