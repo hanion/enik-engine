b684371382fce510973c550b1f71ff95ef6fa8bf
yigithanion <55713249+hanion@users.noreply.github.com>
2023-07-14 21:53:10 +0300

    call layers' OnAttach functions from application

--- engine/src/application.cpp
+++ engine/src/application.cpp
@@ -24,10 +24,12 @@ Application::~Application() {
 
 void Application::PushLayer(Layer* layer) {
 	m_LayerStack.PushLayer(layer);
+	layer->OnAttach();
 }
 
 void Application::PushOverlay(Layer* overlay) {
 	m_LayerStack.PushOverlay(overlay);
+	overlay->OnAttach();
 }
 
 
