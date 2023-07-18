47cdce45d3910e4dcdf5aaf71e93067337dd9bb7
yigithanion <55713249+hanion@users.noreply.github.com>
2023-07-18 19:43:15 +0300

    add imgui to main header

--- engine/include/Enik.h
+++ engine/include/Enik.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "../src/layers/imgui_layer/imgui_layer.h"
+#include "../external/imgui/imgui.h"
 #include "../src/layers/layer.h"
 #include "../src/application.h"
 #include "../src/input.h"
