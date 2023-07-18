bd182e36d7fca477aae28ff38901c48fa3e4cab4
yigithanion <55713249+hanion@users.noreply.github.com>
2023-07-18 13:55:37 +0300

    set glfwSetCharCallback

--- engine/src/window.cpp
+++ engine/src/window.cpp
@@ -103,6 +103,15 @@ void Window::Init(const WindowProperties& properties){
 	);
 
 
+	glfwSetCharCallback(m_Window, 
+		[](GLFWwindow* window, unsigned int keycode){
+			WindowData& data = *(WindowData*)glfwGetWindowUserPointer(window);
+			KeyTypedEvent event(keycode);
+			data.EventCallback(event);
+		}
+	);
+
+
 	glfwSetMouseButtonCallback(m_Window, 
 		[](GLFWwindow* window, int button, int action, int mods){
 			WindowData& data = *(WindowData*)glfwGetWindowUserPointer(window);
