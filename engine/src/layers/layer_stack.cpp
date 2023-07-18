0114316e3f1609559a9a903e44c6b5e90d5dacac
yigithanion <55713249+hanion@users.noreply.github.com>
2023-07-18 18:34:00 +0300

    fix layerstack overflow error
    by making it an index

--- engine/src/layers/layer_stack.cpp
+++ engine/src/layers/layer_stack.cpp
@@ -5,7 +5,6 @@ namespace Enik {
 
 
 LayerStack::LayerStack() {
-	m_LayerInsert = m_Layers.begin();
 }
 
 LayerStack::~LayerStack() {
@@ -15,7 +14,8 @@ LayerStack::~LayerStack() {
 }
 
 void LayerStack::PushLayer(Layer* layer) {
-	m_LayerInsert = m_Layers.emplace(m_LayerInsert, layer);
+	m_Layers.emplace(m_Layers.begin() + m_LayerInsertIndex, layer);
+	m_LayerInsertIndex++;
 }
 
 void LayerStack::PushOverlay(Layer* overlay) {
@@ -27,7 +27,7 @@ void LayerStack::PopLayer(Layer* layer) {
 
 	if (it != m_Layers.end()) {
 		m_Layers.erase(it);
-		m_LayerInsert--;
+		m_LayerInsertIndex--;
 	}
 }
 
