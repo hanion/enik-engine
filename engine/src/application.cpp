c68a8f2c182c1c7b658ae87c77190e3789a81aac
yigithanion <55713249+hanion@users.noreply.github.com>
2023-07-19 17:24:42 +0300

    rendering a triangle setup

--- engine/src/application.cpp
+++ engine/src/application.cpp
@@ -16,6 +16,36 @@ Application::Application() {
 
 	m_ImGuiLayer = new ImGuiLayer();
 	PushOverlay(m_ImGuiLayer);
+
+	// Vertex Array
+	// Vertex Buffer
+	// Index Buffer
+
+	glGenVertexArrays(1, &m_VertexArray);
+	glBindVertexArray(m_VertexArray);
+
+	glGenBuffers(1, &m_VertexBuffer);
+	glBindBuffer(GL_ARRAY_BUFFER, m_VertexBuffer);
+
+	float vertices[3 * 3] = {
+		0.0f,  0.8f,  0.0f,
+		-0.5f, -0.1f, 0.0f,
+		0.5f,  -0.1f, 0.0f
+	};
+
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+	glEnableVertexAttribArray(0);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), nullptr);
+
+	glGenBuffers(1, &m_IndexBuffer);
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, m_IndexBuffer);
+
+	unsigned int indices[3] = { 0, 1, 2 };
+
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
+
 }
 
 Application::~Application() {
@@ -36,6 +66,13 @@ void Application::PushOverlay(Layer* overlay) {
 
 void Application::Run() {
     while (m_Running) {
+
+		glClearColor(0.1f, 0.1f, 0.1f, 1);
+		glClear(GL_COLOR_BUFFER_BIT);
+
+		glBindVertexArray(m_VertexArray);
+		glDrawElements(GL_TRIANGLES, 3, GL_UNSIGNED_INT, nullptr);
+
 		for (Layer* layer : m_LayerStack) {
 			layer->OnUpdate();
 		}
--- engine/src/application.h
+++ engine/src/application.h
@@ -35,6 +35,7 @@ private:
     bool m_Running = true;
     LayerStack m_LayerStack;
 
+    unsigned int m_VertexArray, m_VertexBuffer, m_IndexBuffer;
 private:
     static Application* s_Instance;
 };
