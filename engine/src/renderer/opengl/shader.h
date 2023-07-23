f702270cb03c722de2ffacc9dcef1b65c247c1ed
yigithanion <55713249+hanion@users.noreply.github.com>
2023-07-23 19:43:46 +0300

    add shader class

---
+++ engine/src/renderer/opengl/shader.h
@@ -0,0 +1,18 @@
+#pragma once
+#include <string>
+
+namespace Enik {
+
+class Shader {
+public:
+	Shader(const std::string& vertexSource, const std::string& fragmentSource);
+	~Shader();
+
+	void Bind() const;
+	void Unbind() const;
+
+private:
+	uint m_RendererID;
+};
+
+}
