3f4e0a82b3cd81425d3a4bc5b9ded48d6cf020c2
yigithanion <55713249+hanion@users.noreply.github.com>
2023-08-03 16:44:27 +0300

    remove unnecessary lines from shader

--- sandbox/assets/shaders/flat_color.glsl
+++ sandbox/assets/shaders/flat_color.glsl
@@ -7,11 +7,9 @@ uniform mat4 u_ViewProjection;
 uniform mat4 u_Transform;
 uniform vec4 u_Color;
 
-out vec3 v_Position;
 out vec4 v_Color;
 
 void main() {
-	v_Position = a_Position;
 	v_Color = u_Color;
 	gl_Position = u_ViewProjection * u_Transform * vec4(a_Position, 1.0);
 }
@@ -20,7 +18,6 @@ void main() {
 #version 330 core
 
 layout(location = 0) out vec4 color;
-in vec3 v_Position;
 in vec4 v_Color;
 
 void main() {
