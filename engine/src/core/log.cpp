18a35ecc3c63af49987c28cbb0f14f5571c8338b
yigithanion <55713249+hanion@users.noreply.github.com>
2023-08-14 20:41:26 +0300

    update debug log

--- engine/src/core/log.cpp
+++ engine/src/core/log.cpp
@@ -6,7 +6,7 @@ std::shared_ptr<spdlog::logger> Log::s_ClientLogger;
 
 void Log::Init() {
 	spdlog::set_pattern("%^[%T] %n: %v%$");
-	s_CoreLogger = spdlog::stdout_color_mt("EE");
+	s_CoreLogger = spdlog::stdout_color_mt("EN");
 	s_CoreLogger->set_level(spdlog::level::trace);
 
 	s_ClientLogger = spdlog::stdout_color_mt("APP");
--- engine/src/core/log.h
+++ engine/src/core/log.h
@@ -21,6 +21,7 @@ private:
 };
 }
 
+#ifdef EN_DEBUG
 // Core log macros 
 #define EN_CORE_TRACE(...)  ::Enik::Log::GetCoreLogger()->trace(__VA_ARGS__)
 #define EN_CORE_INFO(...)   ::Enik::Log::GetCoreLogger()->info(__VA_ARGS__)
@@ -33,4 +34,17 @@ private:
 #define EN_WARN(...)   ::Enik::Log::GetClientLogger()->warn(__VA_ARGS__)
 #define EN_ERROR(...)  ::Enik::Log::GetClientLogger()->error(__VA_ARGS__)
 #define EN_FATAL(...)  ::Enik::Log::GetClientLogger()->fatal(__VA_ARGS__)
-
+#else
+// Core log macros 
+#define EN_CORE_TRACE(...)
+#define EN_CORE_INFO(...)
+#define EN_CORE_WARN(...)
+#define EN_CORE_ERROR(...)
+#define EN_CORE_FATAL(...)
+// Client log macros 
+#define EN_TRACE(...)
+#define EN_INFO(...)
+#define EN_WARN(...)
+#define EN_ERROR(...)
+#define EN_FATAL(...)
+#endif
