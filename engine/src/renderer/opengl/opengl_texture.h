b4a53fba4a5849859c467c09414efdf84d298509
yigithanion <55713249+hanion@users.noreply.github.com>
2023-08-14 21:54:14 +0300

    add GetRendererID to texture

--- engine/src/renderer/opengl/opengl_texture.h
+++ engine/src/renderer/opengl/opengl_texture.h
@@ -14,6 +14,7 @@ public:
 
 	virtual uint32_t GetWidth() const override { return m_Width; }; 
 	virtual uint32_t GetHeight() const override { return m_Height; }; 
+	virtual uint32_t GetRendererID() const override { return m_RendererID; }; 
 
 	virtual void Bind(uint32_t slot = 0) const override; 
 
