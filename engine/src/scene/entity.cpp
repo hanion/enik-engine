aa90749d31a580d4b4511265a29f64cf9eb25ef5
yigithanion <55713249+hanion@users.noreply.github.com>
2023-08-26 11:29:54 +0300

    fix spelling of m_Handle

--- engine/src/scene/entity.cpp
+++ engine/src/scene/entity.cpp
@@ -5,7 +5,7 @@ namespace Enik {
 
 
 Entity::Entity(entt::entity handle, Scene* scene) 
-	:m_Hanle(handle), m_Scene(scene) {
+	:m_Handle(handle), m_Scene(scene) {
 	
 }
 
--- engine/src/scene/entity.h
+++ engine/src/scene/entity.h
@@ -14,41 +14,41 @@ public:
 	template<typename T, typename... Args>
 	T& Add(Args&&... args){
 		EN_CORE_ASSERT(!Has<T>(), "Entity already has component!");
-		return m_Scene->m_Registry.emplace<T>(m_Hanle, std::forward<Args>(args)...);
+		return m_Scene->m_Registry.emplace<T>(m_Handle, std::forward<Args>(args)...);
 	}
 	
 	template<typename T>
 	T& Get() {
 		EN_CORE_ASSERT(Has<T>(), "Entity does not have component!");
-		return m_Scene->m_Registry.get<T>(m_Hanle);
+		return m_Scene->m_Registry.get<T>(m_Handle);
 	}
 
 	
 	template<typename T>
 	bool Has() {
-		return m_Scene->m_Registry.all_of<T>(m_Hanle);
+		return m_Scene->m_Registry.all_of<T>(m_Handle);
 	}
 
 	template<typename T>
 	void Remove(){
 		EN_CORE_ASSERT(Has<T>(), "Entity does not have component!");
-		m_Scene->m_Registry.remove<T>(m_Hanle);
+		m_Scene->m_Registry.remove<T>(m_Handle);
 	}
 
-	operator bool() const { return m_Hanle != entt::null; }
+	operator bool() const { return m_Handle != entt::null; }
 
-	operator entt::entity() const { return m_Hanle; }
-	operator uint32_t() const { return (uint32_t)m_Hanle; }
+	operator entt::entity() const { return m_Handle; }
+	operator uint32_t() const { return (uint32_t)m_Handle; }
 
 	bool operator == (Entity other) {
-		return m_Hanle == other.m_Hanle && m_Scene == other.m_Scene;
+		return m_Handle == other.m_Handle && m_Scene == other.m_Scene;
 	}
 	bool operator != (Entity other) {
 		return !(*this == other);
 	}
 
 private:
-	entt::entity m_Hanle{ entt::null };
+	entt::entity m_Handle{ entt::null };
 	Scene* m_Scene = nullptr;
 
 };
