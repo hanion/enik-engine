3e3933c4deb1e9b32aed2976e9c352bbb61dd1a5
yigithanion <55713249+hanion@users.noreply.github.com>
2023-09-12 15:35:49 +0300

    add InspectorExportFloat to extern_functions

--- engine/src/script_system/extern_functions.cpp
+++ engine/src/script_system/extern_functions.cpp
@@ -1,6 +1,7 @@
 #include "extern_functions.h"
 
 #include "script_system/script_system.h"
+#include "../imgui_utils.h"
 
 namespace Enik {
 
@@ -10,6 +11,22 @@ Entity FindEntityByUUID(UUID uuid) {
 
 Entity FindEntityByName(const std::string& name) {
     return ScriptSystem::GetSceneContext()->FindEntityByName(name);
+}
+
+
+void InspectorExportFloat(const std::string& text, float& value, float v_speed, float v_min, float v_max, const char* format) {
+	ImGuiUtils::PrefixLabel(text);
+	ImGui::DragFloat(("##"+text).c_str(), &value, v_speed, v_min, v_max, format);
+}
+
+void InspectorExportFloat2(const std::string& text, float v[2], float v_speed, float v_min, float v_max, const char* format) {
+	ImGuiUtils::PrefixLabel(text);
+	ImGui::DragFloat2(("##"+text).c_str(), v, v_speed, v_min, v_max, format);
+}
+
+void InspectorExportFloat3(const std::string& text, float v[3], float v_speed, float v_min, float v_max, const char* format) {
+	ImGuiUtils::PrefixLabel(text);
+	ImGui::DragFloat3(("##"+text).c_str(), v, v_speed, v_min, v_max, format);
 }
 
 }
