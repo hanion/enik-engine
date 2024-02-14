e08b621580d501bf686797ec967f647f3ce5a16c
yigithanion <55713249+hanion@users.noreply.github.com>
2024-02-14 22:00:04 +0300

    move imgui_utils.h to src/utils/

---
+++ editor/src/utils/imgui_utils.h
@@ -0,0 +1,43 @@
+#pragma once
+#include <base.h>
+#include <imgui/imgui.h>
+#include <imgui/imgui_internal.h>
+
+namespace Enik {
+namespace ImGuiUtils {
+
+inline void PrefixLabel(std::string_view title) {
+	ImGuiWindow* window = ImGui::GetCurrentWindow();
+	const ImGuiStyle& style = ImGui::GetStyle();
+	float full_width = ImGui::GetContentRegionAvail().x;
+	float item_width = ImGui::CalcItemWidth() + style.ItemSpacing.x;
+	ImVec2 text_size = ImGui::CalcTextSize(title.data());
+	ImRect text_rect;
+	text_rect.Min = ImGui::GetCursorScreenPos();
+	text_rect.Max = text_rect.Min;
+	text_rect.Max.x += full_width - item_width;
+	text_rect.Max.y += text_size.y;
+
+	ImGui::SetCursorScreenPos(text_rect.Min);
+
+	ImGui::AlignTextToFramePadding();
+	// Adjust text rect manually because we render it directly into a drawlist instead of using public functions.
+	text_rect.Min.y += window->DC.CurrLineTextBaseOffset;
+	text_rect.Max.y += window->DC.CurrLineTextBaseOffset + 1; // +1 so there is no letters bottom clip
+
+	ImGui::ItemSize(text_rect);
+	if (ImGui::ItemAdd(text_rect, window->GetID(title.data(), title.data() + title.size()))) {
+		ImGui::RenderTextEllipsis(ImGui::GetWindowDrawList(), text_rect.Min, text_rect.Max, text_rect.Max.x,
+			text_rect.Max.x, title.data(), title.data() + title.size(), &text_size);
+
+		if (text_rect.GetWidth() < text_size.x && ImGui::IsItemHovered()) {
+			ImGui::SetTooltip("%.*s", (int)title.size(), title.data());
+		}
+	}
+
+	ImGui::SetCursorScreenPos(ImVec2(text_rect.Max.x, text_rect.Max.y - (text_size.y + window->DC.CurrLineTextBaseOffset)));
+	ImGui::SameLine();
+}
+
+}
+}
--- engine/src/script_system/extern_functions.cpp
+++ engine/src/script_system/extern_functions.cpp
@@ -1,7 +1,6 @@
 #include "extern_functions.h"
 
 #include "script_system/script_system.h"
-#include "../imgui_utils.h"
 #include "physics/physics_world.h"
 
 namespace Enik {
